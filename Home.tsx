9d83a365dc21a6e53b9c089bab21ac041be44dd3
holabisii <ajoseholabisi@gmail.com>
2022-04-23 07:53:43 -0400

    import react-native  error fixed

---
+++ Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text as T, View as V } from 'react-native'
+
+const Home = () => {
+        return (
+                <V>
+
+                        <T style={{color: 'black'}}>
+                                this is my first compoenent in a desktop app
+                        </T>
+
+                </V>
+        )
+}
+
+export default Home;
